ab7d2c69ef7bc036af08d84da82877e07ab3177f
bala <balamurugan.radhakrishnan@yapily.com>
2020-11-26 12:01:45 +0000

    Day5-NotificationsSearchMenu Final

--- 005-notifications-search-and-menu/index.js
+++ 005-notifications-search-and-menu/index.js
@@ -1,8 +1,14 @@
 let notificationMenuElement = document.querySelector(".notification-menu");
 let notificationElement = document.querySelector(".center");
 let menuElement = document.querySelector(".menu");
+let searchIconElement = document.querySelector(".search-icon");
+let searchBoxElement = document.querySelector(".search-item");
 
 notificationMenuElement.addEventListener("click", () => {
   menuElement.classList.toggle("active");
   notificationElement.classList.toggle("inactive");
+});
+
+searchIconElement.addEventListener("click", () => {
+  searchBoxElement.classList.toggle("active");
 });
